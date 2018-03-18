1e1fac56bc124cf881afbf920fa4c359aea1596d
Debreceni Bálint <mrdebb94@gmail.com>
2018-03-18 19:28:14 +0100

    Add coordinate conversions from room drawing

--- src/js/webgl/Scene2D.ts
+++ src/js/webgl/Scene2D.ts
@@ -25,12 +25,13 @@ export class Scene2D {
   
   constructor(width, height, fieldOfViewDegree, zNear, zFar, translateZ) {
 	  
-	 /* this.m_triangles.push ( new Triangle(
+	/* this.m_triangles.push ( new Triangle(
             vec3.fromValues(0.5, 0.5,0.0),
 			vec3.fromValues(-0.5,0.5,0.0),
-			vec3.fromValues( 0.5, -0.5,0.0)
-	       ) );
-		   
+			vec3.fromValues( 0.5, -0.5,0.0),
+			vec4.fromValues( 1.0, 1.0,1.0,1.0),
+	       ) );*/
+	/*	   
 	  this.m_triangles.push ( new Triangle(
             vec3.fromValues(-0.5, -0.5,0.0),
 			vec3.fromValues(-0.5,0.5,0.0),
@@ -109,6 +110,117 @@ export class Scene2D {
 					 [0.0, 0.0, translateZ]);
   }
   
+  
+  convert3DPointToScreen(point:vec4){
+	  
+	 let multipliedMatrix = mat4.create();
+     mat4.multiply(multipliedMatrix,this.m_projectionMatrix,this.m_modelViewMatrix);	
+
+     //Matrix4 viewProjectionMatrix = projectionMatrix * viewMatrix;
+     
+	 let screenPoint = vec4.create();
+     vec4.transformMat4(screenPoint,point, multipliedMatrix);
+      
+	  console.log(screenPoint); 
+	  //point3D = viewProjectionMatrix.multiply(point3D);
+     
+ 	  let winX = Math.round((( (screenPoint[0]/screenPoint[3]) + 1 ) / 2.0) *
+                                   this.m_width );
+     
+      let winY = Math.round((( 1 - screenPoint[1]/screenPoint[3] ) / 2.0) *
+                                   this.m_height );
+				
+      console.log("Vissza " + winX + " " + winY);	
+
+	
+      //return new Point2D(winX, winY);
+  }
+  
+  convert2DPointTo3DWorld(winx,winy):vec3 {
+	  
+	   /*let projectionInverse= mat4.create();
+	  let multipliedMatrix = mat4.create();
+	  mat4.multiply(multipliedMatrix,this.m_modelViewMatrix,this.m_projectionMatrix);
+	  mat4.invert(projectionInverse, multipliedMatrix);*/
+	  
+	  /*
+	  let winz=0;
+	  let x = 2.0 * winx / this.m_width - 1;
+	  let y = - 2.0 * winy / this.m_height + 1;
+      let z = 2*winz-1;
+	  
+	  let projectionInverse= mat4.create();
+	  mat4.invert(projectionInverse, this.m_projectionMatrix);
+	  
+	  let point3D = vec4.create();
+	  let pointWorldA = vec4.create();
+		
+	  vec4.set(point3D,x,y,z,1.0);
+	  vec4.transformMat4(pointWorldA,point3D,projectionInverse);
+	  
+	  pointWorldA[0] = pointWorldA[0]*(z/pointWorldA[3]);
+	  pointWorldA[1] = pointWorldA[1]*(z/pointWorldA[3]);
+	  pointWorldA[2] = pointWorldA[2]*(z/pointWorldA[3]);
+	  pointWorldA[3]= 1.0;
+	  
+	  console.log("VegeA");
+	  console.log(pointWorldA);
+	  
+	  winz=1;
+	  z = 2*winz-1;
+	  
+	  point3D = vec4.create();
+	  let pointWorldB = vec4.create();
+		
+	  vec4.set(point3D,x,y,z,1.0);
+	  vec4.transformMat4(pointWorldB,point3D,projectionInverse);
+	  
+	  pointWorldB[0] = pointWorldB[0]*(z/pointWorldB[3]);
+	  pointWorldB[1] = pointWorldB[1]*(z/pointWorldB[3]);
+	  pointWorldB[2] = pointWorldB[2]*(z/pointWorldB[3]);
+	  pointWorldB[3]= 1.0;
+	  
+	  console.log("VegeB");
+	  console.log(pointWorldB);
+	
+	  let rayDirection = vec3.create();
+	  vec3.subtract(rayDirection,vec3.fromValues(pointWorldB[0],  pointWorldB[1],  pointWorldB[2]), 
+	  vec3.fromValues(pointWorldA[0],  pointWorldA[1],  pointWorldA[2]));
+		
+	  let normalizedRayDirection = vec3.create()
+		
+	  vec3.normalize(normalizedRayDirection, rayDirection);  
+	  
+	  return vec3.fromValues(normalizedRayDirection[0]*5, normalizedRayDirection[1]*5, 0);
+	  
+	  */
+	  let imageAspectRatio = this.m_width / this.m_height; // width > height 
+	  let Px = (2 * ((winx + 0.5) / this.m_width) - 1) * Math.tan(this.m_fieldOfViewDegree / 2 * Math.PI / 180) * this.m_aspectRatio; 
+	  let Py = (1 - 2 * ((winy + 0.5) / this.m_height)) * Math.tan(this.m_fieldOfViewDegree/ 2 * Math.PI / 180); 
+    
+	  //console.log((Px*5) + " " + (Px*5) + " ");
+	  
+	  let rayOrigin = vec3.fromValues(0,0,0);
+	  
+	  //console.log(rayOrigin);
+	   
+      let rayDirection = vec3.create();
+	  vec3.subtract(rayDirection,vec3.fromValues(Px, Py, -1), rayOrigin);
+	  
+	  console.log(Math.sqrt(Px*Px + Py*Py + 1));
+		
+	  let normalizedRayDirection = vec3.create()
+		
+	  vec3.normalize(normalizedRayDirection, rayDirection); 
+	
+      //return vec3.fromValues(Px*5, Py*5, 0);
+	  console.log(Math.sqrt(normalizedRayDirection[0]*normalizedRayDirection[0] + 
+	  normalizedRayDirection[1]*normalizedRayDirection[1]+normalizedRayDirection[2]*normalizedRayDirection[2]));
+	  
+	  //world-be vagyunk, a modellre vissza kell alakítani
+	  return vec3.fromValues(normalizedRayDirection[0]*5, normalizedRayDirection[1]*5, (normalizedRayDirection[2]*5)+5);
+  }
+  
   getRayTo2DPoint(x,y):vec3[] {
 	 
 		//let fov = 45;
@@ -171,6 +283,10 @@ export class Scene2D {
   
   get rooms():Room[] {
 	    return this.m_rooms;
+  }
+  
+  addTriangle(t:Triangle) {
+	  this.m_triangles.push(t);
   }
   
 
